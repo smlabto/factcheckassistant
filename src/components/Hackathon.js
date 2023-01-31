4887317d5b64ed7423303520f54a5b060deb5729
Anatoliy Gruzd <agruzd@gmail.com>
2023-01-31 18:40:09 -0500

    scale hackathon card to the width of the screen

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -12,14 +12,10 @@ function About() {
                     <div class="row">
                         <center>
                             <a href="https://socialmedialab.ca/events/hackathon/" target="_blank">
-                                <img src="https://i0.wp.com/socialmedialab.ca/wp-content/uploads/2023/01/AI-Misinfo-Hack-1-1920x1080.jpg?resize=768%2C432&amp;ssl=1"></img> 
+                                <img width="100%" src="https://i0.wp.com/socialmedialab.ca/wp-content/uploads/2023/01/AI-Misinfo-Hack-1-1920x1080.jpg?resize=768%2C432&amp;ssl=1"></img> 
                             </a>
                         </center>
                     </div>
-                </div>
-                <div class="text-center mt-4">
-                    <a class="btn btn-xl btn-outline-light" href="https://github.com/smlabto/factcheckassistant" rel="noreferrer" target="_blank">
-                        <i class="fas fa-download me-2"></i>Get Source Code!</a>
                 </div>
             </div>
         </section>        
