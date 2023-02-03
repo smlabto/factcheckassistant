481aedfb812f0e4f7b3e820cef5f3305b659f936
Anatoliy Gruzd <agruzd@gmail.com>
2023-02-03 14:32:52 -0500

    updates to info boxes

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -10,6 +10,10 @@ function About() {
                 </div>
                 <div class="column">
                     <div class="row">
+                        <p>The Social Media Lab's Fact Check Assistant is created as a demonstration for the <a class="link-warning" href="https://socialmedialab.ca/events/hackathon/" rel="noreferrer" target="_blank">the Social Media Lab's 2023 Canadian #AI Misinformation Hackathon</a>. Its purpose is to show and assess the potential of AI in doing simple fact-checking. It was built using OpenAI API and Bootstrap.
+                        </p>
+                    </div>
+                    <div class="row">
                         <center>
                             <a href="https://socialmedialab.ca/events/hackathon/" rel="noreferrer" target="_blank">
                                 <img class="img-fluid rounded" width="70%" alt="Social Media Lab AI Misinfo Hackathon Promo" src="https://i0.wp.com/socialmedialab.ca/wp-content/uploads/2023/01/AI-Misinfo-Hack-1-696x392.jpg"></img> 
