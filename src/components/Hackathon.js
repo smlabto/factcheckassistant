96f9e93909f7fb0f5709cfa99c3a7e1da1fd5b6a
tiago-a-ribeiro <tiagoribeiro64625@gmail.com>
2023-02-15 15:48:14 -0500

    Update to higher res banner

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -16,7 +16,7 @@ function About() {
                     <div class="row">
                         <center>
                             <a href="https://socialmedialab.ca/events/hackathon/" rel="noreferrer" target="_blank">
-                                <img class="img-fluid rounded" width="70%" alt="Social Media Lab AI Misinfo Hackathon Promo" src="https://i0.wp.com/socialmedialab.ca/wp-content/uploads/2023/01/AI-Misinfo-Hack-1-696x392.jpg"></img> 
+                                <img class="img-fluid rounded" width="70%" alt="Social Media Lab AI Misinfo Hackathon Promo" src="https://i0.wp.com/socialmedialab.ca/wp-content/uploads/2023/01/AI-Misinfo-Hack-4-1920x1080.jpg"></img> 
                             </a>
                         </center>
                     </div>
